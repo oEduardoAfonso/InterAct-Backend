12b9a886ea700905f6b3b89657b6baa59e2b6603
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-09-29 18:20:34 -0300

    Adicionando configuracao da api

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
+from flask_restful import Api
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 
+api = Api(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from .models import usuario_model, sala_model, pergunta_model
