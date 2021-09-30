857214ff888d672f488c53a51d211b6d670cad00
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-09-29 21:46:34 -0300

    Adicionando Schema de usuario

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 from flask_restful import Api
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -9,6 +10,8 @@ app.config.from_object('config')
 
 api = Api(app)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
+
 
 from .views import usuario_view, sala_view, pergunta_view
