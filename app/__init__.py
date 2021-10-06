ffb56bd64100ab26f4bd0b10806aac0e466b99c0
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-06 18:49:23 -0300

    Adicionando CORS

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -12,5 +13,6 @@ api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+CORS(app)
 
 from .views import usuario_view, sala_view, pergunta_view
