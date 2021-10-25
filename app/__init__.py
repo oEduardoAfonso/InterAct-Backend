329e142d9f7f66f1ed7a597193d73c6ab9018527
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-10-25 14:38:50 -0300

    Adicionando socket

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate, migrate
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ app.config.from_object('config')
 api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+io = SocketIO(app)
 migrate = Migrate(app, db)
 CORS(app)
 
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
-from app import app
+from app import app, io
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    io.run(app, host='0.0.0.0', port=port)
